5e433d2386d997711b0c3bebded1e1d104543407
helewud <helewud@gmail.com>
2025-02-20 17:52:35 +0100

    chore: setup style guides

--- quiz-app/src/index.tsx
+++ quiz-app/src/index.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import App from "./App";
+import "./assets/styles/global.scss";
+import "normalize.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
