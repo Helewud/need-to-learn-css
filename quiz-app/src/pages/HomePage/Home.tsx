1469c49307639f196888ed1bb7f303b0412b3e43
helewud <helewud@gmail.com>
2025-02-25 10:29:21 +0100

    chore: refactor and add context providers to pages and use dynamuc data

--- quiz-app/src/pages/HomePage/Home.tsx
+++ quiz-app/src/pages/HomePage/Home.tsx
@@ -1,95 +1,61 @@
-import { useEffect, useState } from "react";
-import {
-  AccessibilityIcon,
-  CssIcon,
-  HtmlIcon,
-  JsIcon,
-} from "../../components/core/Icon";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { GetIconComponentByCategory } from "../../components/core/Icon/Icon";
 import HomeSelectButton from "../../components/features/home/HomeSelectButton";
 import Layout from "../../components/features/layout/Layout";
-import { QuizCategoryProvider } from "../../context/QuizContext";
+import { QuizContext } from "../../context/QuizContext";
 import { fetchQuiz } from "../../services/api";
 import { Quiz, QuizCategory } from "../../types";
 import styles from "./Home.module.scss";
 
 export const Home = () => {
-  const [quizMap, setQuizMap] = useState<Map<string, Quiz>>(() => {
-    const saved = window.localStorage.getItem("quiz");
-    return saved ? JSON.parse(saved) : null;
-  });
-
-  const [quizCategory, setQuizCategory] = useState("");
+  const navigate = useNavigate();
+  const { setQuiz } = useContext(QuizContext);
+  const [quizData, setQuizData] = useState<Quiz[]>();
 
   useEffect(() => {
-    (async function () {
-      const q = await fetchQuiz();
+    (async () => {
+      const resp = await fetchQuiz();
+      if (resp instanceof Error) return;
 
-      if (q instanceof Error) {
-        return;
-      }
-
-      const newQuizMap = new Map<string, Quiz>();
-      q.every((qu) => newQuizMap.set(qu.title, qu));
-
-      if (newQuizMap.size) {
-        window.localStorage.setItem("quiz", JSON.stringify(newQuizMap));
-        setQuizMap(newQuizMap);
-      }
+      setQuizData(resp);
     })();
   }, []);
 
-  const quizCategories: string[] = [];
+  if (!quizData) return;
 
-  if (quizMap?.size) {
-    for (const key of quizMap.keys()) {
-      quizCategories.push(key);
-    }
-  }
+  const handleQuizCategory = (e: React.MouseEvent) => {
+    const target = e.target as HTMLButtonElement;
+    setQuiz(quizData.find((q) => q.title === target.value)!);
+    navigate("/question");
+  };
 
   return (
-    <QuizCategoryProvider
-      value={{ category: quizCategory, setCategory: setQuizCategory }}
-    >
-      <Layout>
-        <div className={styles["content"]}>
-          <section className={styles["header"]}>
-            <h2>
-              <span className={styles["minor-header"]}>Welcome to the</span>
-              <span className={styles["major-header"]}>Frontend Quiz!</span>
-            </h2>
-            <p>Pick a subject to get started.</p>
-          </section>
+    <Layout>
+      <div className={styles.content}>
+        <section className={styles.header}>
+          <h2>
+            <span className={styles["minor-header"]}>Welcome to the</span>
+            <span className={styles["major-header"]}>Frontend Quiz!</span>
+          </h2>
+          <p>Pick a subject to get started.</p>
+        </section>
 
-          <div className={styles["category-options"]}>
-            {quizCategories.map((category, index) => {
-              const icon = getQuizCategoryIconComponent(category as never);
-              return (
-                <HomeSelectButton
-                  key={index}
-                  category={category}
-                  children={icon}
-                />
-              );
-            })}
-          </div>
+        <div className={styles["category-options"]}>
+          {quizData.map(({ title }, index) => {
+            const icon = GetIconComponentByCategory(title as QuizCategory);
+            return (
+              <HomeSelectButton
+                key={index}
+                category={title}
+                clickAction={handleQuizCategory}
+              >
+                {icon}
+              </HomeSelectButton>
+            );
+          })}
         </div>
-      </Layout>
-    </QuizCategoryProvider>
+      </div>
+    </Layout>
   );
-};
-
-const getQuizCategoryIconComponent = (category: QuizCategory) => {
-  switch (category) {
-    case "HTML":
-      return <HtmlIcon />;
-
-    case "CSS":
-      return <CssIcon />;
-
-    case "JavaScript":
-      return <JsIcon />;
-
-    case "Accessibility":
-      return <AccessibilityIcon />;
-  }
 };
--- quiz-app/src/pages/ScorePage/Score.tsx
+++ quiz-app/src/pages/ScorePage/Score.tsx
@@ -1,18 +1,46 @@
-import { IconAndNameGroup, JsIcon } from "../../components/core/Icon";
-import { Layout } from "../../components/features/layout";
-import styles from "./Score.module.scss";
-import { ScoreCard } from "../../components/features/score/ScoreCard";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router";
 import { Button } from "../../components/core/Button";
+import { IconAndNameGroup } from "../../components/core/Icon";
+import { GetIconComponentByCategory } from "../../components/core/Icon/Icon";
+import { Layout } from "../../components/features/layout";
+import { ScoreCard } from "../../components/features/score/ScoreCard";
+import { QuizContext } from "../../context/QuizContext";
+import { QuizCategory } from "../../types";
+import styles from "./Score.module.scss";
 
 export const Score = () => {
-  const cateogory = (
-    <IconAndNameGroup name="JavaScript">
-      <JsIcon />
-    </IconAndNameGroup>
-  );
+  const navigate = useNavigate();
+  const { quiz } = useContext(QuizContext);
+
+  useEffect(() => {
+    if (!quiz || !quiz.title || !quiz?.questions?.length) {
+      navigate("/");
+      return;
+    }
+  }, [quiz, navigate]);
+
+  if (!quiz || !quiz.title || !quiz?.questions?.length) {
+    return;
+  }
+
+  const category = quiz.title;
+  const questions = quiz.questions;
+  let correctCount = 0;
+  const totalCount = quiz.questions.length;
+
+  questions?.forEach((q) => q.answer === q.selection && correctCount++);
+
+  const handleRetryQuiz = () => {
+    navigate("/");
+  };
+
+  const icon = GetIconComponentByCategory(category as QuizCategory);
+
+  const iconGroup = <IconAndNameGroup name={category}>{icon}</IconAndNameGroup>;
 
   return (
-    <Layout category={cateogory}>
+    <Layout category={iconGroup}>
       <div className={styles["content"]}>
         <h2 className={styles["header"]}>
           <span className={styles["minor-header"]}>Quiz Completed</span>
@@ -20,11 +48,12 @@ export const Score = () => {
         </h2>
 
         <div className={styles["score-content"]}>
-          <ScoreCard correctCount={8} totalCount={20}>
-            {cateogory}
+          <ScoreCard correctCount={correctCount} totalCount={totalCount}>
+            {iconGroup}
           </ScoreCard>
 
-          <Button text="Play Again" />
+          <Button clickAction={handleRetryQuiz} text="Play Again" />
+          <Button clickAction={handleRetryQuiz} text="Review Solution" />
         </div>
       </div>
     </Layout>
